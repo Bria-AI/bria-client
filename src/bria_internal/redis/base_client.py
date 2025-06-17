67ded420f4aff0413ae8fec5bab01a8c6a013c15
omri <omriassa@bria.ai>
2025-06-17 18:23:39 +0300

    fix: redis key method

--- src/bria_internal/redis/base_client.py
+++ src/bria_internal/redis/base_client.py
@@ -3,6 +3,7 @@
 
 
 import pickle
+from abc import ABC, abstractmethod
 from typing import Any, Optional
 
 from redis import Redis
@@ -12,17 +13,21 @@ from src.bria_internal.common.singleton_meta import SingletonMeta
 DEFAULT_TTL = 3600
 
 
-class RedisBaseClient(metaclass=SingletonMeta):
+class RedisBaseClient(ABC, metaclass=SingletonMeta):
 
     def __init__(self, host: str, port: int = 6379):
         assert host is not None, "redis host cannot be None"
         self.client = Redis(host=host, port=port, socket_timeout=10)
 
+    @abstractmethod
+    def _get_key(self, key):
+        return key
+
     def set(self, key: str, value: Any, ttl: int = DEFAULT_TTL):
-        self.client.set(key, self.__serialize(value), ex=ttl)
+        self.client.set(self._get_key(key), self.__serialize(value), ex=ttl)
 
     def get(self, key: str) -> Optional[Any]:
-        data = self.client.get(key)
+        data = self.client.get(self._get_key(key))
         if data is not None:
             return self.__deserialize(data)
         return None
