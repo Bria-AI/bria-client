1e85dcc523f1257f23d209cef035c9c484b92a79
omri <omriassa@bria.ai>
2025-11-11 19:56:28 +0100

    fix: exclude internal from build

---
+++ src/bria_client/internal/internal_request_client.py
@@ -0,0 +1,30 @@
+from bria_client.clients.engine_client import EngineClient
+from bria_client.exceptions.engine_api_exception import ContentModerationException, EngineAPIException
+from bria_client.schemas.image_editing_apis import ContentModeratedPayloadModel, PromptContentModeratedPayloadModel
+
+
+class InternalEngineClient(EngineClient):
+
+    @property
+    def _headers(self):
+        return {"X-Internal-Request": True}
+
+    def get_custom_exception(self, e: EngineAPIException, payload: ContentModeratedPayloadModel) -> ContentModerationException | EngineAPIException:
+        """
+        Converting the Broader EngineAPIException to the more specific custom exceptions models.
+
+        Args:
+            `e: EngineAPIException` - The exception to convert
+            `payload: ContentModeratedPayloadModel` - The payload that was used to make the request
+
+        Returns:
+            `ContentModerationException` - If the request is not suitable for content moderation
+
+            `EngineAPIException` - If the request is not suitable for content moderation
+        """
+        if e.response.status_code == 422:
+            if isinstance(payload, ContentModeratedPayloadModel) and payload.is_moderated:
+                return ContentModerationException.from_engine_api_exception(e)
+            if isinstance(payload, PromptContentModeratedPayloadModel) and payload.is_moderated:
+                return ContentModerationException.from_engine_api_exception(e)
+        return e
