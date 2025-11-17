0c3b06337cb42cccd9e66061a1ef99938a3e5b6b
assaomri <omriassa@bria.ai>
2025-11-17 14:06:57 +0100

    feat: added base_url to BriaClient (#26)

--- src/bria_client/__init__.py
+++ src/bria_client/__init__.py
@@ -10,6 +10,7 @@ from bria_client.engine_client import BriaEngineClient
 class BriaClient:
     def __init__(
         self,
+        base_url: str | None = None,
         api_token_ctx: ContextVar[str] | str | None = None,
         jwt_token_ctx: ContextVar[str] | str | None = None,
         retry: Retry | None = None,
@@ -19,6 +20,8 @@ class BriaClient:
         if isinstance(jwt_token_ctx, str):
             jwt_token_ctx = ContextVar("bria_engine_jwt_token", default=jwt_token_ctx)
 
-        self._client = BriaEngineClient(api_token_ctx, jwt_token_ctx, retry=retry)
+        self._client = BriaEngineClient(base_url=base_url,
+                                        api_token_ctx=api_token_ctx, jwt_token_ctx=jwt_token_ctx,
+                                        retry=retry)
         self.status = StatusAPI(self._client)
         self.image_editing = ImageEditingAPI(self._client, self.status)
