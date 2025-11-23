1f79654f22467b9ab5750d3f5881838c016bd583
Lev David <lev@bria.ai>
2025-11-23 17:15:21 +0200

    fix video payload

--- src/bria_client/schemas/video_apis/video_generation.py
+++ src/bria_client/schemas/video_apis/video_generation.py
@@ -2,6 +2,6 @@ from bria_client.schemas.base_models import APIPayloadModel
 
 
 class VideoGenerationByTailoredImageRequestPayload(APIPayloadModel):
-    tailored_model_id: str
+    model_id: str
     image: str
     seed: int | None = None
