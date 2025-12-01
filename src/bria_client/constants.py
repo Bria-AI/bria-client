9837f83a841e30dd812c9f26a252f28ce87a95b6
Lev David <lev@bria.ai>
2025-12-01 14:59:53 +0200

    spacing

--- src/bria_client/constants.py
+++ src/bria_client/constants.py
@@ -11,7 +11,9 @@ BRIA_ENGINE_PRODUCTION_URL: Final[str] = "https://engine.prod.bria-api.com/"
 
 class BriaEngineAPIRoutes(StrEnum):
     V1_IMAGE_EDIT_GET_MASKS = "v1/objects/mask_generator"
+
     V2_STATUS = "v2/status"
+
     V2_IMAGE_EDIT_REMOVE_BACKGROUND = "v2/image/edit/remove_background"
     V2_IMAGE_EDIT_ERASER = "v2/image/edit/erase"
     V2_IMAGE_EDIT_GEN_FILL = "v2/image/edit/gen_fill"
@@ -22,7 +24,9 @@ class BriaEngineAPIRoutes(StrEnum):
     V2_IMAGE_EDIT_ENHANCE_IMAGE = "v2/image/edit/enhance"
     V2_IMAGE_EDIT_INCREASE_RESOLUTION = "v2/image/edit/increase_resolution"
     V2_IMAGE_EDIT_CROP_FOREGROUND = "v2/image/edit/crop_foreground"
+
     V2_VIDEO_GENERATE_BY_TAILOR_IMAGE = "v2/video/generate/tailored/image-to-video"
+    V2_VIDEO_GENERATE_FOREGROUND_MASK = "v2/video/generate/foreground_mask"
+
     V2_VIDEO_EDIT_INCREASE_RESOLUTION = "v2/video/edit/increase_resolution"
     V2_VIDEO_EDIT_REMOVE_BACKGROUND = "v2/video/edit/remove_background"
-    V2_VIDEO_GENERATE_FOREGROUND_MASK = "v2/video/generate/foreground_mask"
