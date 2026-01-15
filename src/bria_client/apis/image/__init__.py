3e7d9148e82a0f7bad8477d28796c72860cafac2
Noam Aghai <noam@bria.ai>
2026-01-15 11:22:20 +0200

    Updated the imageapi class

--- src/bria_client/apis/image/__init__.py
+++ src/bria_client/apis/image/__init__.py
@@ -1,8 +1,5 @@
 from bria_client.apis.image.generation import ImageGenerationAPI
-from bria_client.apis.status import StatusAPI
-from bria_client.engine_client import BriaEngineClient
 
 
-class ImageAPI:
-    def __init__(self, engine_client: BriaEngineClient, status_api: StatusAPI):
-        self.generation = ImageGenerationAPI(engine_client, status_api)
+class ImageAPI(ImageGenerationAPI):
+    pass
