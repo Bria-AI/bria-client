3a3a60ce5b36a5fdfa59a696a5ae514c480de48e
omri <omriassa@bria.ai>
2026-01-19 10:49:53 +0200

    fix: tests

--- src/bria_client/decorators/__init__.py
+++ src/bria_client/decorators/__init__.py
@@ -1,3 +1,3 @@
-from bria_client.decorators.temp_api_token import with_temp_api_token
+from bria_client.decorators.temp_api_token import async_with_temp_api_token, with_temp_api_token
 
-__all__ = ["with_temp_api_token"]
+__all__ = ["with_temp_api_token", "async_with_temp_api_token"]
